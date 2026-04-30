fb74512b7e4120ef4330460ab4ceb99dee850cb3
yungwolsky <mikolajwolskinauka@gmail.com>
2026-04-30 13:59:10 +0200

    Add webhook payload and PayU order

---
+++ Share&Care/models/requests/PayUWebhookPayload.cs
@@ -0,0 +1,15 @@
+﻿using Share_Care.Models.Requests;
+
+namespace Share_Care.models.requests
+{
+    public class PayUWebhookPayload
+    {
+        public PayUOrder? Order {  get; set; }
+    }
+
+    public class PayUOrder
+    {
+        public string OrderId { get; set; } = null;
+        public string Status { get; set; } = null;
+    }
+}
