d4fec8706a6e699066770667ec3dd4a3a86bcf5c
yungwolsky <mikolajwolskinauka@gmail.com>
2026-04-30 19:14:12 +0200

    Add helpers for tests

---
+++ Share&Care/Share&Care.Tests/Helpers/TestData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Share_Care.models;
+
+namespace Share_Care.Tests.Helpers
+{
+    public static class TestData
+    {
+        public static Wallet Wallet(decimal balance = 100)
+            => new Wallet { UserId = "user1", Balance = balance };
+
+        public static Transaction Transaction(decimal amount = 100)
+            => new Transaction
+            {
+                Id = "tx1",
+                UserId = "user1",
+                Amount = amount,
+                Status = "Pending"
+            };
+
+        public static Escrow Escrow()
+            => new Escrow
+            {
+                Id = "esc1",
+                BorrowerId = "borrower",
+                LenderId = "lender",
+                OfferId = "offer1",
+                Amount = 50,
+                Status = "Locked"
+            };
+    }
+}
