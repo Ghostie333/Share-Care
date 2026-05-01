e1a93e8055dff2132bf2a49260de04ccf2acefa2
yungwolsky <mikolajwolskinauka@gmail.com>
2026-05-01 15:20:28 +0200

    Add HttpClient to Program.cs

--- Share&Care/Program.cs
+++ Share&Care/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
+builder.Services.AddHttpClient();
 
 
 // MongoDB
