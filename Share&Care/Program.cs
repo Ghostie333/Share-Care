cee1e7422466fcdf691065247c3ce34d1de7e0bc
yungwolsky <mikolajwolskinauka@gmail.com>
2026-05-03 16:15:55 +0200

    Modify Http redirecting in Program.cs

--- Share&Care/Program.cs
+++ Share&Care/Program.cs
@@ -37,8 +37,14 @@ builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<IChatService, ChatService>();
 builder.Services.AddScoped<IWalletService, WalletService>();
 builder.Services.AddScoped<IEscrowService, EscrowService>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
+
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddHttpClient<IPaymentService, PaymentService>()
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        AllowAutoRedirect = false
+    });
 
 // AUTH: JWT dla Flutter (Web + Mobile)
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
